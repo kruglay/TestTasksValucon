86490aa20688367d24f2e919cd677f61c35baf42
kruglay <kruglay@mail.ru>
2017-04-06 13:15:41 +0300

    VC2 change .gitignore

--- lib/trello_api/test.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'trello_board'
-# p File.realpath('../../')
-# p TrelloBoard.public_class_method
-b = TrelloBoard.new('58ded028055c3564f08ab422')
-card = b.create_card!(title: "22sometitle22", description: "somedescription", list_id: "58ded035b5a575cdb375a48b")
-
-p b
-p card
